674412ec50b070ab90ece2a6e8f843ee70f9e423
mrmthornton <mrmthornton@gmail.com>
2014-11-30 17:35:26 -0800

    test web form fill

--- webFormFill.py
+++ webFormFill.py
@@ -1,38 +1,18 @@
-#-------------------------------------------------------------------------------
-# Name:        module1
-# Purpose:
-#
-# Author:      IEUser
-#
-# Created:     21/11/2014
-# Copyright:   (c) IEUser 2014
-# Licence:     <your licence>
-#-------------------------------------------------------------------------------
 
 #import django.http.request as requests
 #import lxml.html as lh
 
 import urllib
 import urllib2
+import webbrowser
 
-urllib.urlretrieve("http://github.com/login/", "somefile.html", lambda x,y,z:0, urllib.urlencode({"username": "mrmthornton", "password": "Publix5418Git"}))
-hold = '''
-gh_url = 'https://api.github.com'
+url = 'http://duckduckgo.com/html'        # target url
+data = urllib.urlencode({'q':'Python'})   # put string Python in field q
+results = urllib2.urlopen(url,data)
+with open('page.html', 'w') as pageFile:
+    pageFile.write(results.read())
 
-req = urllib2.Request(gh_url)
+webbrowser.open('page.html')
 
-password_manager = urllib2.HTTPPasswordMgrWithDefaultRealm()
-password_manager.add_password(None, gh_url, ' mrmthornton', 'Publix5418Git')
-
-auth_manager = urllib2.HTTPBasicAuthHandler(password_manager)
-opener = urllib2.build_opener(auth_manager)
-urllib2.install_opener(opener)
-handler = urllib2.urlopen(req)
-
-print handler.getcode()
-print handler.headers.getheader('content-type')
-'''
 
 # see stackoverflow 'using python to sign into website,
-# fill in a form, then sign out
-
