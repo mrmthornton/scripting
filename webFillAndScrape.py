e3c451bd5e53cc06b46ed0dd36f30a433316a932
mrmthornton <mrmthornton@gmail.com>
2014-12-04 15:40:58 -0600

    get web page text

--- webFillAndScrape.py
+++ webFillAndScrape.py
@@ -9,33 +9,37 @@ import urllib2
 import webbrowser
 from lxml import html
 
-url = 'https://duckduckgo.com/html'       # target URL
-data = urllib.urlencode({'q':'Python'})   # html form of 'q gets value Python'
+url = 'https://simple.wikipedia.org/wiki/Main_Page'
+
+
+#url = 'https://duckduckgo.com/html'       # target URL
+#data = urllib.urlencode({'q':'Python'})   # html form of 'q gets value Python'
 
 #url = 'https://accounts.google.com/ServiceLoginAuth'       # target URL
 #data = urllib.urlencode({'Email':'mrmthornton@gmail.com', 'Passwd':'randomGmail'})
 #data = urllib.urlencode({'Email':'mrmthornton@gmail.com'})
 
-request = urllib2.Request(url, data)      # formulate a request
+request = urllib2.Request(url)
+#request = urllib2.Request(url, data)      # formulate a request
 results = urllib2.urlopen(request)        # open URL with DATA, get html
 content = results.read()                  # get string version of page
 # save content as web page
 with open('page.html', 'w') as pageFile:  # open the file to store html
     pageFile.write(content)               # write to file
 # view content in browser
-webbrowser.open('page.html')              # view the html
+##webbrowser.open('page.html')              # view the html
 
 # extract text portion of page
-snippet = ''
-#
-#while True:
-#    snippetStartPattern = re.compile('<div class="snippet">')
-#    foundStart = snippetStartPattern.search(content)
-#    if foundStart != None:
-#        content = content[foundStart.end():]
-#        snippetEndPattern = re.compile('</div>')
-#        foundEnd = snippetEndPattern.search(content)
-#        if foundEnd != None:
+snippetStartPattern = re.compile('title="Welcome!">')
+foundStart = snippetStartPattern.search(content)
+if foundStart != None:
+    content = content[foundStart.end():]
+    snippetEndPattern = re.compile('</div>')
+    foundEnd = snippetEndPattern.search(content)
+    if foundEnd != None:
+        content = content[:foundEnd.start()]
+        print content
+
 #            snippet = content[:foundEnd.start() + 1]
 #            snippet.replace('<b>', '')
 #            print snippet
