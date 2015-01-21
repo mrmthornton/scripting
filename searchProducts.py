4993bc82441281b2f6d6950f982b389581660796
mrmthornton <mrmthornton@gmail.com>
2015-01-21 15:11:10 -0600

    using page search form

--- searchProducts.py
+++ searchProducts.py
@@ -5,16 +5,16 @@
 # close the page
 
 # works on win7, ie10
-
+import os
 from selenium import webdriver
 
-
+#create an instance of IE and set some options
 driver = webdriver.Ie()
-
 driver.implicitly_wait(30)
 driver.maximize_window()
 
-url = 'http://demo.magentocommerce.com/'
+url = 'http://www.magentocommerce.com/magento-connect/live-demo.html'
+#url = 'http://www.magentocommerce.com/demo'
 driver.get(url)
 
 searchField = driver.find_element_by_name('q')
@@ -23,7 +23,9 @@ searchField.clear()
 searchField.send_keys("phones")
 searchField.submit()
 
-products = driver.find_elements_by_xpath("//h2[@class='product-name']/a")
+products = driver.find_elements_by_xpath("//h2[@class='featured-extension-title']/a")
+#products = driver.find_elements_by_xpath("//h2[@class='product-name']/a")
+
 
 print "Found " + str(len(products)) + ' products:'
 
