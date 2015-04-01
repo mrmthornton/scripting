2c36753c34d35b91dadc34b140303824dffc385e
mrmthornton <mrmthornton@gmail.com>
2015-04-01 15:20:35 -0500

    open CSV file

--- hntb.py
+++ hntb.py
@@ -27,12 +27,14 @@ elem.send_keys(Keys.RETURN)
 #assert 'No results found.' not in driver.page_source
 assert 'search yielded no results' not in driver.page_source
 
-
 # absolute tag forllowed by relative tag
 #selectMenuItem = driver.find_element_by_xpath('//li[@class="mailbox"]/a[contains(@href, "Junk")]')
 
+#elem = driver.find_element_by_link_text("Diversity");
+#elem.click();
+
 try:
-    element = WebDriverWait(driver, 10).until(EC.presence_of_element_located(By.ID, "VPS"))
+    element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable(driver.find_element_by_link_text("Diversity")))
 finally:
     driver.quit()
 
