60e85859dfd822997b9ec5ed393fd5356a915638
mrmthornton <mrmthornton@gmail.com>
2015-05-15 15:36:48 -0500

    hntb:write text to file

--- hntb.py
+++ hntb.py
@@ -1,16 +1,19 @@
-# generic
+# use HNTB site search, putting the search results in 'results.txt'
 # get url,
-# wait for element
-# fill element
-# do next element
-# display for inspection and wait for confirmation
+# test for no results
+# write results to file
 
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 import selenium.webdriver.support.expected_conditions as EC
-#from selenium.webdriver.support.select import Select
+
+import string
+
+def timeout():
+    print "Took too much time!"
+    quit()
 
 driver = webdriver.Ie()
 driver.maximize_window()
@@ -23,41 +26,27 @@ try:
     locator = (By.NAME, "search_block_form")
     element = WebDriverWait(driver, delay).until(EC.presence_of_element_located(locator))
 except TimeoutException:
-    print "Took too much time!"
-    quit()
+    timeout()
 
 searchKey = "career"
 element.send_keys(searchKey)
 element.submit()
 
-print "! Success !"
-
 try:
     locator = (By.XPATH, '//p[@class="search-snippet"]')
-
     results = WebDriverWait(driver, delay).until(EC.presence_of_all_elements_located(locator))
 except TimeoutException:
-    print "Took too much time!"
-    quit()
+    timeout()
 
 for e in results:
-    print e.text
+    s = filter(lambda x: x in string.printable, e.text)
+    print s
+
+with open('results.txt', 'w') as pageFile:  # open the file to store html
+    for e in results:
+        pageFile.write(filter(lambda x: x in string.printable, e.text))        # write to file
 
 #try:
-#    locator = (By.XPATH, "//a[@href='/careers/diversity']")
-#    element = WebDriverWait(driver, delay).until(EC.presence_of_element_located(locator))
-#except TimeoutException:
-#    print "Took too much time!"
-#    quit()
-
-#element.click()
-
-#html = driver.page_source
-
-#with open('page.html', 'w') as pageFile:  # open the file to store html
-#    pageFile.write(html)        # write to file
-
+#    driver.get("file://results.txt") # the get does not display anything
 #webbrowser.open('page.html')              # view the html
-
-
 # see stackoverflow 'using python to sign into website,
