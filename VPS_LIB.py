dfad1dfd0376e6a29027c8002c0bf656686d075d
Michael Thornton <mrmthornton@gmail.com>
2015-12-04 18:56:10 -0600

    Update VPS_LIB.py

--- VPS_LIB.py
+++ VPS_LIB.py
@@ -9,6 +9,7 @@
 # Copyright:   (c) michael thornton 2015
 #-------------------------------------------------------------------------------
 
+from contextlib import contextmanager
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchFrameException
 from selenium.common.exceptions import NoSuchWindowException
@@ -135,7 +136,8 @@ def newPageIsLoaded(driver, delay, currentElement):
             #return False          # loop until timeout occurs
         try:
             # poll the current element with an arbitrary call
-            nullText = currentElement.text
+            WebDriverWait(driver, delay).until(EC.staleness_of(currentElement))
+            test = currentElement.find_elements_by_id('doesnt_matter')
             return False
         except StaleElementReferenceException:
             return True
