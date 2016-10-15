c52bdf91e61fa732c14c8a88dc45bd25f9791618
mrmthornton <mrmthornton@gmail.com>
2016-10-14 20:23:12 -0500

    checking state first

--- VPS_LP_Change.py
+++ VPS_LP_Change.py
@@ -68,12 +68,15 @@ def vps_body(driver, parameters):
             rawString = row[0]
             if rawString == "" or rawString == 0:  #end when first input does not exist
                 break
-            plateString = cleanUpString(rawString)
+            plateString = cleanUpString(rawString) # list[0]
             rawString = row[1]
             if rawString == "" or rawString == 0:  #end when second input does not exist
                 break
-            replacementString = cleanUpString(rawString)
-
+            replacementString = cleanUpString(rawString) # list[1]
+            rawString = row[2]
+            if rawString == "" or rawString == 0:
+                rawString = ""
+            correctState = cleanUpString(rawString) # list[2]
             #select from Violation Status menu
             menuLocator = (By.XPATH, '//select[@name="P_L_VST_VIOL_STATUS_DESCR"]')
             menuElement = findElementOnPage(driver, delay, menuLocator)
@@ -95,7 +98,7 @@ def vps_body(driver, parameters):
                 foundFrame = findAndSelectFrame(driver, delay, "fraRL")
                 #time.sleep(1)  #text may not be there yet!  how long to wait?
                 text = getTextResults(driver, delay, plateString, parameters, "fraRL")
-                if text is not None and text != 0: # there's more to correct
+                if text is not None and text != 0: # there's more to correct   ############ does this do what I think it does?
                     #click on the first record
                     element = findElementOnPage(driver, delay, parameters['LpLocator'])
                     element.click()
@@ -103,13 +106,18 @@ def vps_body(driver, parameters):
                     handle = driver.current_window_handle
                     driver.switch_to_window(handle)
                     foundFrame = findAndSelectFrame(driver, delay, "fraVF")
-                    element = findElementOnPage(driver, delay, parameters['inputLpLocator'])
-                    submitted = fillFormAndSubmit(driver, startWindow, element, replacementString, parameters)
-                    #loop
-                    time.sleep(1)  #page may not be there yet!  how long to wait?
-                    handle = driver.current_window_handle
-                    driver.switch_to_window(handle)
-                    continue
+                    #
+                    # Verify state and ask for user input in not correct?
+                    stateElement = findElementOnPage(driver, delay, parameters['inputStateLocator'] )
+                    select = Select(stateElement)
+                    if select.first_selected_option.text == correctState:
+                        print select.first_selected_option.text
+                        element = findElementOnPage(driver, delay, parameters['inputLpLocator'])
+                        submitted = fillFormAndSubmit(driver, startWindow, element, replacementString, parameters)
+                        time.sleep(1)  #page may not be there yet!  how long to wait?
+                        handle = driver.current_window_handle
+                        driver.switch_to_window(handle)
+                        continue
                 break
             handle = driver.current_window_handle
             driver.switch_to_window(handle)
@@ -131,4 +139,5 @@ if __name__ == '__main__':
     waitForUser()
     vps_body(driver, parameters)
     driver.close()
+    driver.quit()
 
