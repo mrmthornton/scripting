1f1182a9a2800a4acac938c6cf04a3021a05c5e8
mrmthornton <mrmthornton@gmail.com>
2016-11-23 19:07:06 -0600

    working through proxy, no bash environmental variables, only git environmental variables

--- gitvar.sh
+++ gitvar.sh
@@ -1,4 +1,4 @@
 export GIT_CURL_VERBOSE=1
-export GIT_SSL_NO_VERIFY=1
-export GIT_HTTP_USER_AGENT="User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko"
-export GIT_TRACE=1
+#export GIT_SSL_NO_VERIFY=1
+#export GIT_HTTP_USER_AGENT="User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko"
+#export GIT_TRACE=1
