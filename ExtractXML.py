8470b9cf466e4a29bd17dffd70a025c5a1df8b5c
mrmthornton <mrmthornton@gmail.com>
2017-02-24 20:19:17 -0600

    more xml extract

--- ExtractXML.py
+++ ExtractXML.py
@@ -14,38 +14,29 @@
 
 
 class ExtractXML():
-    init (self, startText, rootElementName, lines)
+    def __init__(self, startText, rootElementName, file):
+        self.start = startText
+        self.stop = "</" + rootElementName
+        self.filename = file
 
-		try:
-			while ((line = reader.readLine()) != null) {
-				if (line.indexOf(startText) > -1)
-					break;
-			}
-			while ((line = reader.readLine()) != null) {
-				System.out.println(line);
-				if (line.indexOf("</" + rootElementName) > -1)
-					break;
-		catch (IOException err) {
-			System.err.println(err.getMessage());
-		}
-		finally {
-			if (reader != null)
-				reader.close();
-		}
+    def extract(self):
+        try:
+            with open(filename) as xmlFile:
+                while xmlFile.readline() != self.start:
+                    continue
+                while xmlFile.readline() != self.stop:
+				    print(line)
+        except:
+            println("file not found")
+
+
 
 import argparse
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+    parser.add_argument("inputFile")
     args = parser.parse_args()
-
- 	if (args.length != 1)
-		System.err.println("Usage: ExtractXML <fname>");
-	else {
-		String filename = args[0]
-        extractXML("USCTbankruptcynotice", "ebn:EBNBatch", filename)
-
-#        = [ line.strip() for line in open(filename)]
-#			System.err.println(args[0] + " does not exist");
-#		catch (Exception err) {
-#				System.err.println("main:", err.getMessage());
+    filename = args.inputFile
+    xml = ExtractXML("USCTbankruptcynotice", "ebn:EBNBatch", filename)
+    xml.extract()
