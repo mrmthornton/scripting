4b6e315351d277110d99e93de353cf6d81093aaf
mrmthornton <mrmthornton@gmail.com>
2017-03-01 18:27:18 -0600

    refactor as pythonic loop

--- ExtractXML.py
+++ ExtractXML.py
@@ -1,42 +1,39 @@
 # -*- coding: UTF-8 -*-
 #-------------------------------------------------------------------------------
-# Name:        TxDotTestQuery.py
+# Name:        ExtractXML.py
 #
-# Purpose: *	Reads a PDF file line by line until reaches a specified "start text".
-#               Then prints	each subsequent line until the specifed root element name closing tag is reached.
+# Purpose:     Reads a PDF file line by line until "start" text is found.
+#              Prints each subsequent line until the "stop" text is found.
 #
 # Author:      mthornton, based on ExtractXML.java by satchwinston BAE Systems
 #
 # Created:     2017 FEB 22
-# Update:
+# Update:      2017 MAR 01
 # Copyright:   (c) mthornton 2017
 #-------------------------------------------------------------------------------
 
 import sys
 
-class ExtractXML():
-    def __init__(self, startText, rootElementName, file):
-        self.start = startText
-        self.stop = "</" + rootElementName
-        self.filename = file
-
-    def extract(self):
+def extractXML(start, stop, filename):
         try:
-            with open(filename) as xmlFile, open("./tempfile",'w') as outfile:
-                while self.start in xmlFile.readline():
-                    break
+            with open(filename) as xmlFile:
+                while start not in xmlFile.readline():
+                    continue
+                extracted = []
+                #while stop not in xmlFile.readline():
+                #    sys.stdout.write(line)
+                #    extracted.append(line)
                 while True:
                     line = xmlFile.readline()
                     sys.stdout.write(line)
-                    outfile.write(line)
-                    if self.stop in line:
+                    extracted.append(line)
+                    if stop in line:
                         break
+                return extracted
         except:
             print("file not found")
 
         sys.stdout.flush()
-        #outfile.flush()
-        outfile.close()
 
 
 import argparse
@@ -44,8 +41,9 @@ import argparse
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("inputFile")
+    parser.add_argument("outputFile")
     args = parser.parse_args()
-    filename = args.inputFile
-    xml = ExtractXML("USCTbankruptcynotice", "ebn:EBNBatch", filename)
-    #xml = ExtractXML("USCTbankruptcynotice", "ebn", filename)
-    xml.extract()
+
+    xml = extractXML("USCTbankruptcynotice", "</ebn:EBNBatch", args.inputFile)
+    with open(args.outputFile, 'w') as outfile:
+        outfile.writelines(xml)
