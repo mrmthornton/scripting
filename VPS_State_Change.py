d27933489850f5b9730380422a3d8468d8aac1f8
mrmthornton <mrmthornton@gmail.com>
2017-05-19 19:05:51 -0500

    using vID #s to walk the list

--- VPS_State_Change.py
+++ VPS_State_Change.py
@@ -1,17 +1,14 @@
 #-------------------------------------------------------------------------------
-# Name:        VPS_LP_Count_Images.py
-# Purpose:     Examine web pages until the VPS Violation Search page is found.
-#              enter a licence plate in the search box
-#              find the text number of images
-#              write the licence plate and number of images to the output file.
+# Name:        VPS_State_Change.py
+# Purpose:     Accept input from LP_Change,
+#              a violation list, wrong ST (opt), correct ST
+#              correct each violator, write to donefile
 #
 # Author:      mthornton
 #
 # Created:     2015 AUG 01
-# Updates:     2016 MAR 03
-# Copyright:   (c) michael thornton 2015,2016
-# input(s):    (see parameters, dataInFileName)
-# output(s):   (see parameters, dataOutFileName)
+# Updates:     2017 MAY 19
+# Copyright:   (c) michael thornton 2015,2016, 2017
 #-------------------------------------------------------------------------------
 
 from selenium import webdriver
@@ -31,30 +28,7 @@ import string
 import time
 
 
-def violatorSearch():
-    parameters = {
-    'delay' : 15,
-    'url' : 'https://lprod.scip.ntta.org/scip/jsp/SignIn.jsp', # initial URL
-    #'url' : 'http://intranet/SitePages', # initial URL
-    'operatorMessage' : "Use debug mode, open VPS, new violator search window, and run to completion",
-    'inputLpLocator' : (By.XPATH, '//input[@id = "P_LIC_PLATE_NBR"]'),
-    'inputStateLocator' : (By.XPATH, '//select[@id = "P_LIC_PLATE_STATE"]'),
-    'LpLocator' : (By.XPATH, '//td[@id = "LIC_PLATE_NBR1"]'),
-    'staleLocator' : (By.XPATH,'//h1[contains(text(),"Violation Search")]'),
-    'pageLocator2' : (By.XPATH,'//h1[contains(text(),"Violation Search Result")]'),
-    'buttonLocator' : (By.XPATH,'//input[@value="Query"]'),
-    'frameParamters' : {'useFrames' : True, 'frameLocator' : (By.XPATH, '//frame[@name="fraRxL"]') },
-    'resultPageTextLocator' : (By.XPATH, '//TD/H1'),
-    'resultPageVerifyText' : 'Violation Search Results',
-    'outputLocator' : (By.XPATH,'//BODY/P[contains(text(),"Record")]'),
-    'resultIndexParameters' : {'regex' : "Records \d+ to \d+ of (\d+)", 'selector' : 'tail'},  # head, tail, or all
-    'dataInFileName' : 'LP_correction.csv',
-    'dataOutFileName' : 'LP_correction.txt',
-    'returnOrClick' : 'return', # use Return or Click to submit form
-    }
-    return parameters
-
-def dataIO(driver, parameters):
+def changeStateOnly(driver, parameters):
     delay = parameters['delay']
     startPageTextLocator = (By.XPATH, '//TD/H1[contains(text(),"Violation Search")]')
     # pause on next line for entry of credentials, and window navigation.
